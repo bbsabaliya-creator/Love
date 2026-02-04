f6cf9e83ddc21e7e9a08cea3d209a4bd8ba98c84
bbsabaliya-creator <bbsabaliya@gmail.com>
2026-02-04 10:15:20 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,86 @@
+const CONFIG = {
+
+  name: "My Beautiful Angel 💖",
+
+  letter: `
+My Love ❤️,
+
+Every day with you
+feels like Valentine’s Day.
+
+Thank you for choosing me,
+loving me,
+and completing me.
+
+Forever Yours,
+Bhargav 💍💘
+`,
+
+  week: {
+
+    "02-07": {
+      name: "Rose Day 🌹",
+      emoji: "🌹",
+      symbol: "🌹",
+      title: "Happy Rose Day My Love",
+      final: "My love blooms for you 🌹💖"
+    },
+
+    "02-08": {
+      name: "Propose Day 💍",
+      emoji: "💍",
+      symbol: "💍",
+      title: "Happy Propose Day My Love",
+      final: "Will you be mine forever? 💍💘"
+    },
+
+    "02-09": {
+      name: "Chocolate Day 🍫",
+      emoji: "🍫",
+      symbol: "🍫",
+      title: "Happy Chocolate Day My Sweetheart",
+      final: "You are sweeter than chocolate 🍫💖"
+    },
+
+    "02-10": {
+      name: "Teddy Day 🧸",
+      emoji: "🧸",
+      symbol: "🧸",
+      title: "Happy Teddy Day My Cutie",
+      final: "You are my favorite teddy 🧸💘"
+    },
+
+    "02-11": {
+      name: "Promise Day 🤞",
+      emoji: "⭐",
+      symbol: "⭐",
+      title: "Happy Promise Day My Love",
+      final: "I promise you my forever 🤞💖"
+    },
+
+    "02-12": {
+      name: "Hug Day 🤗",
+      emoji: "🤗",
+      symbol: "💞",
+      title: "Happy Hug Day My Life",
+      final: "My arms are your home 🤗❤️"
+    },
+
+    "02-13": {
+      name: "Kiss Day 😘",
+      emoji: "💋",
+      symbol: "💋",
+      title: "Happy Kiss Day My Heart",
+      final: "Your kisses are my addiction 😘💖"
+    },
+
+    "02-14": {
+      name: "Valentine Day ❤️",
+      emoji: "❤️",
+      symbol: "❤️",
+      title: "Happy Valentine’s Day My Forever",
+      final: "You are my today, tomorrow, always ❤️💍"
+    }
+
+  }
+};
